ad66601ac0fc333aa31bb12e11535fe8bbb56245
yubin11890 <yubin11890@gmail.com>
2024-12-03 23:25:54 +0900

    ini t

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, redirect, url_for
 
-class GPSApp:
-    def __init__(self, import_name):
-        """
-        Flask 앱 초기화 및 설정
-        :param import_name: Flask 앱 이름
-        """
-        self.app = Flask(import_name)
-        CORS(self.app)
-        self.setup_routes()
+app = Flask(__name__)
 
-    def setup_routes(self):
-        """Flask 라우트 정의"""
-        @self.app.route('/')
-        def home():
-            return render_template('index.html')  # 클라이언트에 HTML 파일 제공
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-        @self.app.route('/gps', methods=['POST'])
-        def receive_gps():
-            data = request.get_json()
-            latitude = data.get('latitude')
-            longitude = data.get('longitude')
-            print(f"Received GPS coordinates: Latitude={latitude}, Longitude={longitude}")
-            return jsonify({'status': 'success', 'latitude': latitude, 'longitude': longitude})
+@app.route('/an_data')
+def data():
+    return render_template('an_data.html')
 
-            
-
-    def run(self, host='0.0.0.0', port=5000, debug=True):
-        """Flask 서버 실행"""
-        self.app.run(host=host, port=port, debug=debug, ssl_context=('cert.pem', 'key.pem'))
-
-
-# 애플리케이션 실행
 if __name__ == '__main__':
-    gps_app = GPSApp(__name__)
-    gps_app.run()
+    app.run(debug=True)
